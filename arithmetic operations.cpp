6d0f999d8ed2ec1efd3f154fdb1b383db1df6134
KARNAMMOUNIKA <151985239+KARNAMMOUNIKA@users.noreply.github.com>
2023-12-01 14:47:56 +0530

    Add files via upload

---
+++ arithmetic operations.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,c,d,e,f,g;
+  cout<<"enter the values of a and b";
+  cin>>a>>b;
+  c=a+b;
+  d=a-b;
+  e=a*b;
+  f=a/b;
+  g=a%b;
+  cout<<c<<"\n";
+  cout<<d<<"\n";
+  cout<<e<<"\n";
+  cout<<f<<"\n";
+  cout<<g<<"\n";
+  
+  return 0;
+}
